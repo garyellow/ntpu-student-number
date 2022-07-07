8d0029ffe3437cc4012ce2cfaa0b8cbba1fcf2c0
黃冠穎 <gary20011110@gmail.com>
2022-07-07 23:16:34 +0800

    improve UI

--- student.py
+++ student.py
@@ -1,4 +1,6 @@
+import math
 import msvcrt
+import os
 import random
 import time
 
@@ -7,44 +9,29 @@ from bs4 import BeautifulSoup as BS4
 from fake_useragent import UserAgent
 
 d = {
-    '法律': '71', '法學': '712', '司法': '714', '財法': '716',
-    '公行': '72',
-    '經濟': '73',
-    '社會': '74', '社學': '742', '社工': '744',
-    '財政': '75',
-    '不動': '76',
-    '會計': '77',
-    '統計': '78',
-    '企管': '79',
-    '金融': '80',
-    '中文': '81',
-    '應外': '82',
-    '歷史': '83',
-    '休運': '84',
-    '資工': '85',
-    '通訊': '86',
-    '電機': '87'
+    '法律': 71, '法學': 712, '司法': 714, '財法': 716,
+    '公行': 72,
+    '經濟': 73,
+    '社會': 74, '社學': 742, '社工': 744,
+    '財政': 75,
+    '不動': 76,
+    '會計': 77,
+    '統計': 78,
+    '企管': 79,
+    '金融': 80,
+    '中文': 81,
+    '應外': 82,
+    '歷史': 83,
+    '休運': 84,
+    '資工': 85,
+    '通訊': 86,
+    '電機': 87
 }
 
-D = {
-    '71': '法律', '712': '法學', '714': '司法', '716': '財法',
-    '72': '公行',
-    '73': '經濟',
-    '74': '社會', '742': '社學', '744': '社工',
-    '75': '財政',
-    '76': '不動',
-    '77': '會計',
-    '78': '統計',
-    '79': '企管',
-    '80': '金融',
-    '81': '中文',
-    '82': '應外',
-    '83': '歷史',
-    '84': '休運',
-    '85': '資工',
-    '86': '通訊',
-    '87': '電機'
-}
+
+def get_key(value):
+    return str([k for k, v in d.items() if v == value]).strip("[']")
+
 
 print('歡迎使用依學號查詢姓名程式')
 print('本程式可用學號查到北大日間部學生之姓名')
@@ -55,9 +42,11 @@ while True:
     ua = UserAgent()
     header_seed = ua.random
     header = {'user-agent': header_seed}
-    f = open('save.txt', 'w', encoding='utf-8')
 
     mode = input('1.查詢個人\n2.查詢全系\n請選擇查詢模式\n>> ')
+    while mode not in ['1', '2']:
+        print('\n請輸入 1 或 2\n')
+        mode = input('1.查詢個人\n2.查詢全系\n請選擇查詢模式\n>> ')
 
     while True:
         if mode == '1':
@@ -66,8 +55,8 @@ while True:
             if not number.isdecimal():
                 print('\n請輸入數字!!!\n')
                 continue
-            if int(number) > 999999999 or int(number) < 99999999:
-                print('\n請輸入正確學號!!!\n')
+            if int(number) > 500000000 or int(number) < 40000000:
+                print('\n請輸入正確格式!!!\n')
                 continue
 
             url = 'http://lms.ntpu.edu.tw/portfolio/search.php?fmScope=2&fmKeyword=' + number
@@ -79,7 +68,6 @@ while True:
 
             try:
                 print('\n' + number + ' ' + name.text + '\n\n')
-                f.write(str(number + ' ' + name.text + '\n'))
             except AttributeError:
                 print('\n學號' + number + '不存在')
                 print('請重新輸入學號\n')
@@ -88,114 +76,85 @@ while True:
             break
 
         elif mode == '2':
-            year = input('\n請輸入入學年度\n>> ')
-
-            if not year.isdecimal():
-                print('\n請輸入數字!!!\n')
+            try:
+                year = int(input('\n請輸入入學年度\n>> '))
+            except ValueError:
+                print('\n請輸入數字!!')
                 continue
-            if int(year) >= 2022:
-                print('\n你未來人???\n')
-                continue
-            if int(year) < 50:
-                print('\n你原始人???\n')
-                continue
-            if 110 < int(year) < 1911:
-                print('\n請輸入正確年分!!!\n')
-                continue
-            if int(year) >= 1911:
-                year = str(int(year) - 1911)
 
-            department = input('\n請輸入科系名稱或編號\n>> ')
-            if department.isdecimal():
-                if not 71 <= int(department) <= 87 and int(department) != 712 and int(department) != 714 and int(
-                        department) != 716 and int(department) != 742 and int(department) != 744:
-                    print('\n請輸入正確科系編號!!!\n')
-                    continue
-                elif int(department) == 71:
-                    print('\n法律系請輸入科系+組別編號!!!\n')
-                    continue
-                elif int(department) == 74:
-                    print('\n社會系請輸入科系+組別編號!!!\n')
-                    continue
+            if year >= 1911:
+                year -= 1911
 
-                DPM = '4' + '{:0>3d}'.format(int(year)) + department
-
-                print()
-                for i in range(1, 999):
-                    if int(department) <= 99:
-                        student_number = DPM + '{:0>3d}'.format(i)
-                    else:
-                        student_number = DPM + '{:0>2d}'.format(i)
-
-                    URL = 'http://lms.ntpu.edu.tw/portfolio/search.php?fmScope=2&fmKeyword=' + student_number
-                    web = requests.get(URL, headers=header)
-                    web.encoding = 'utf-8'
-
-                    html = BS4(web.text, 'html.parser')
-                    name = html.find('div', {'class': 'bloglistTitle'})
-
-                    try:
-                        print(student_number + ' ' + name.text)
-                        f.write(str(student_number + ' ' + name.text + '\n'))
-                    except AttributeError:
-                        print('\n' + year + '學年度' + D[department] + '系共有' + str(i - 1) + '個學生\n')
-                        break
-
-                    time.sleep(random.uniform(0, 0.2))
+            if year > time.localtime(time.time()).tm_year:
+                print('\n你未來人??\n')
+                break
+            elif year < 90:
+                print('\n學校都還沒蓋好，急什麼XD\n')
+                break
+            elif year < 95:
+                print('\n資料未建檔\n')
                 break
 
-            else:
+            while True:
+                department = input('\n請輸入科系名稱或編號\n>> ')
                 try:
-                    d[department]
-                except KeyError:
-                    print('\n請輸入正確科系名稱(不須加「系」)')
+                    department = int(department)
+                except ValueError:
+                    try:
+                        department = d[department.rstrip('系')]
+                    except KeyError:
+                        print('\n請輸入正確科系名稱或編號!!')
+                        continue
+
+                if department == d['法律']:
+                    print('\n法律系請輸入法學、司法、財法或完整科系編號~')
+                    continue
+                elif department == d['社會']:
+                    print('\n社會相關科系請輸入社學、社工或完整科系編號~')
+                    continue
+                elif department not in d.values():
+                    print('\n請輸入正確科系名稱或編號!!')
                     continue
 
-                if d[department] == '71':
-                    case = input('\n1.法學\n2.司法\n3.財法\n請確認您的組別\n>> ')
-                    while case != '1' and case != '2' and case != '3':
-                        print('\n請輸入1、2或是3\n')
-                        case = input('\n1.法學\n2.司法\n3.財法\n請確認您的組別\n>> ')
-                    DPM = '4' + '{:0>3d}'.format(int(year)) + d[department] + str(int(case) * 2)
-
-                elif d[department] == '74':
-                    case = input('\n1.社學\n2.社工\n請確認您的系別\n>> ')
-                    while case != '1' and case != '2':
-                        print('\n請輸入1或2\n')
-                        case = input('\n1.社學\n2.社工\n請確認您的系別\n>> ')
-                    DPM = '4' + '{:0>3d}'.format(int(year)) + d[department] + str(int(case) * 2)
-
-                else:
-                    DPM = '4' + '{:0>3d}'.format(int(year)) + d[department]
-
-                print()
-                for i in range(1, 999):
-                    if int(d[department]) <= 99 and int(d[department]) != 71 and int(d[department]) != 74:
-                        student_number = DPM + '{:0>3d}'.format(i)
-                    else:
-                        student_number = DPM + '{:0>2d}'.format(i)
-
-                    URL = 'http://lms.ntpu.edu.tw/portfolio/search.php?fmScope=2&fmKeyword=' + student_number
-                    web = requests.get(URL, headers=header)
-                    web.encoding = 'utf-8'
-
-                    html = BS4(web.text, 'html.parser')
-                    name = html.find('div', {'class': 'bloglistTitle'})
-
-                    try:
-                        print(student_number + ' ' + name.text)
-                        f.write(str(student_number + ' ' + name.text + '\n'))
-                    except AttributeError:
-                        print('\n' + year + '學年度' + department + '系共有' + str(i - 1) + '個學生\n')
-                        break
-
-                    time.sleep(random.uniform(0, 0.2))
                 break
-        else:
-            print('\n請輸入1或2\n')
-            mode = input('1.查詢個人\n2.查詢全系\n請選擇查詢模式\n>> ')
 
-    f.close()
+            if not os.path.isdir('student'):
+                os.mkdir('student')
+            f = open('student/' + str(year) + '學年度' + (get_key(department) + '系' if math.floor(department / 10) != d['法律'] else get_key(int(department / 10)) + '系' + get_key(department) + '組') + '學生名單.txt', 'w+', encoding='utf-8')
+
+            DPM = '4' + str(year) + str(department)
+
+            print()
+            for i in range(1, 999):
+                if department <= 99:
+                    student_number = DPM + '{:0>3d}'.format(i)
+                else:
+                    student_number = DPM + '{:0>2d}'.format(i)
+
+                URL = 'http://lms.ntpu.edu.tw/portfolio/search.php?fmScope=2&fmKeyword=' + student_number
+                web = requests.get(URL, headers=header)
+                web.encoding = 'utf-8'
+
+                html = BS4(web.text, 'html.parser')
+                name = html.find('div', {'class': 'bloglistTitle'})
+
+                try:
+                    print(student_number + ' ' + name.text)
+                    f.write(str(student_number + ' ' + name.text + '\n'))
+                except AttributeError:
+                    if math.floor(department / 10) == d['法律']:
+                        print('\n' + str(year) + '學年度' + get_key(department / 10) + '系' + get_key(department) + '組共有' + str(i - 1) + '個學生\n')
+                        f.write(str(year) + '學年度' + get_key(department / 10) + '系' + get_key(department) + '組共有' + str(i - 1) + '個學生\n')
+                    else:
+                        print('\n' + str(year) + '學年度' + get_key(department) + '系共有' + str(i - 1) + '個學生\n')
+                        f.write(str(year) + '學年度' + get_key(department) + '系共有' + str(i - 1) + '個學生\n')
+
+                    f.close()
+                    break
+
+                time.sleep(random.uniform(0, 0.2))
+
+            break
 
     print('按Enter鍵重新查詢，其他鍵離開程式')
     if ord(msvcrt.getch()) != 13:
